f38fe7904154b96df204b2221f546beb0abb8ca0
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2020-07-11 12:12:23 -0400

    Hide cmd window when running Windows script for launching app locally

--- launch-app-locally.bat
+++ launch-app-locally.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%~dp0index.html" --allow-file-access-from-files
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%~dp0index.html" --allow-file-access-from-files
