6b96b3a8152eb96094c34a15118c28a7888ea099
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-03-24 23:18:24 -0400

    Update Windows script for launching the app locally

--- launch-app-locally.bat
+++ launch-app-locally.bat
@@ -1,1 +1,1 @@
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%~dp0index.html" --allow-file-access-from-files
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "%~dp0index.html" --allow-file-access-from-files
