f4e3d3d12ffac3d7565b3e85fc4fe617d317af16
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-03-31 17:00:08 -0400

    Use a pure Python method for computing feed diffs

--- extra/feed-archive/step-3-diff-feeds.py
+++ extra/feed-archive/step-3-diff-feeds.py
@@ -4,7 +4,7 @@ Run a diff command on each successive pair of parsed feed JSON files.
 
 import os
 import re
-import sys
+import difflib
 
 
 for input_dir in ['critical-role', 'nerdist']:
@@ -23,8 +23,13 @@ for input_dir in ['critical-role', 'nerdist']:
     dates = list(file_list.keys())
     for i in range(1, len(dates)):
         old_date, new_date = dates[i - 1], dates[i]
-        old_feed = os.path.join(input_dir, file_list[old_date])
-        new_feed = os.path.join(input_dir, file_list[new_date])
+        old_feed_path = os.path.join(input_dir, file_list[old_date])
+        new_feed_path = os.path.join(input_dir, file_list[new_date])
         output_file = os.path.join(output_dir, f'feed-diff-{new_date}.diff')
-        command = f'diff -u10 {old_feed} {new_feed} > {output_file}'
-        os.popen(command)
+        with open(old_feed_path, 'r') as f:
+            old_feed = f.readlines()
+        with open(new_feed_path, 'r') as f:
+            new_feed = f.readlines()
+        diff = difflib.unified_diff(old_feed, new_feed, fromfile=old_feed_path, tofile=new_feed_path, n=10)
+        with open(output_file, 'w') as f:
+            f.write(''.join(diff))
