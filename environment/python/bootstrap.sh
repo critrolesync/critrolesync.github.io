560b1793a99d48702e6776b830a84711bd688046
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-09-16 19:47:16 -0400

    Patch dejavu to provide some additional logging
    insert_hashes is the slowest operation, so it helps to see when progress is made.

--- environment/python/bootstrap.sh
+++ environment/python/bootstrap.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install -y python3.7 python3-pip ffmpeg libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 python3-psycopg2
 
 python3 -m pip install -r /vagrant/environment/python/requirements.txt
+patch /usr/local/lib/python3.8/dist-packages/dejavu/__init__.py /vagrant/environment/python/dejavu-logging.patch
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
