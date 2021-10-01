579a228bbae05693defc6db41bc9c59bcf59ae3f
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2021-09-30 20:15:10 -0400

    Slightly improve podcast feed archival scripts

--- feed-archive/step-1-fetch-feeds.py
+++ feed-archive/step-1-fetch-feeds.py
@@ -1,20 +1,19 @@
 """
-Retrieve RSS feeds for archival
+Retrieve podcast RSS feeds for archival in XML format
 """
 
-import os
-import datetime
+from datetime import date
+from pathlib import Path
 import urllib.request
 
 
 feeds = {
     'critical-role': 'https://anchor.fm/s/91c7948/podcast/rss',
-    'nerdist': 'http://critrole.nerdistind.libsynpro.com/rss',
+    'nerdist':       'http://critrole.nerdistind.libsynpro.com/rss',
     # 'geek-and-sundry': 'https://criticalrole.podbean.com/feed.xml',  # taken down as of 2020-07-08
 }
 
 # download each feed and save it as an XML file with today's date
 for name, url in feeds.items():
-    filename = os.path.join(name, f'feed-{datetime.date.today()}.xml')
-    if not os.path.exists(filename):
-        urllib.request.urlretrieve(url, filename)
+    filename = Path(name) / f'feed-{date.today()}.xml'
+    urllib.request.urlretrieve(url, filename)
--- feed-archive/step-3-diff-feeds.py
+++ feed-archive/step-3-diff-feeds.py
@@ -1,35 +1,33 @@
 """
-Run a diff command on each successive pair of parsed feed JSON files.
+Generate a diff report for each successive pair of parsed feed JSON files
 """
 
-import os
-import re
 import difflib
+from pathlib import Path
+import re
 
 
 for input_dir in ['critical-role', 'nerdist']:
-    input_dir = os.path.join(input_dir, 'parsed')
-    output_dir = os.path.join(input_dir, 'diffs')
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
+    input_dir = Path(input_dir) / 'parsed'
+    output_dir = input_dir / 'diffs'
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     file_list = {}
-    for filename in os.listdir(input_dir):
-        match = re.fullmatch('feed-parsed-(.*)\.json', filename)
+    for file in input_dir.glob('feed-parsed-*.json'):
+        match = re.fullmatch('feed-parsed-(.*)\.json', file.name)
         if match:
             date = match.group(1)
-            file_list[date] = filename
+            file_list[date] = file
 
-    dates = list(file_list.keys())
-    for i in range(1, len(dates)):
-        old_date, new_date = dates[i - 1], dates[i]
-        old_feed_path = os.path.join(input_dir, file_list[old_date])
-        new_feed_path = os.path.join(input_dir, file_list[new_date])
-        output_file = os.path.join(output_dir, f'feed-diff-{new_date}.diff')
+    dates = sorted(file_list.keys())
+    for old_date, new_date in zip(dates[:-1], dates[1:]):
+        old_feed_path = file_list[old_date]
+        new_feed_path = file_list[new_date]
+        output_file = output_dir / f'feed-diff-{new_date}.diff'
         with open(old_feed_path, 'r') as f:
             old_feed = f.readlines()
         with open(new_feed_path, 'r') as f:
             new_feed = f.readlines()
-        diff = difflib.unified_diff(old_feed, new_feed, fromfile=old_feed_path, tofile=new_feed_path, n=10)
+        diff = difflib.unified_diff(old_feed, new_feed, fromfile=str(old_feed_path), tofile=str(new_feed_path), n=10)
         with open(output_file, 'w') as f:
             f.write(''.join(diff))
