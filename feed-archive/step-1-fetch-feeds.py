9f13e8de2153b3d402b93d50a4177622ef8bf90c
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2023-04-13 12:40:04 -0400

    Rename original G&S podcast feed archive

--- feed-archive/step-1-fetch-feeds.py
+++ feed-archive/step-1-fetch-feeds.py
@@ -11,7 +11,7 @@ import urllib.request
 feeds = {
     'critical-role': 'https://anchor.fm/s/91c7948/podcast/rss',
     'nerdist':       'http://critrole.nerdistind.libsynpro.com/rss',
-    # 'geek-and-sundry': 'https://criticalrole.podbean.com/feed.xml',  # taken down as of 2020-07-08
+    # 'original-geek-and-sundry': 'https://criticalrole.podbean.com/feed.xml',  # taken down as of 2020-07-08
 }
 
 # download each feed and save it as an XML file with today's date
